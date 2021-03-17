ecc8d71880a7d21cb148d87c0d06c57fe3c11740
Hansatang <portokarlos@gmail.com>
2021-03-17 09:48:07 +0100

    Pacierz 09:39

--- src/Client1/ViewModel/ViewModelFactory.java
+++ src/Client1/ViewModel/ViewModelFactory.java
@@ -2,4 +2,14 @@ package Client1.ViewModel;
 
 public class ViewModelFactory
 {
+  private ViewModelLogin viewModelLogin;
+  private ViewModelChat viewModelChat;
+
+  public ViewModelLogin getViewModelLogin() {
+    return viewModelLogin;
+  }
+
+  public ViewModelChat getViewModelChat() {
+    return viewModelChat;
+  }
 }
