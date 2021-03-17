0b71f7b7ff4f34ad7787b4c3a1ce958ffcedf4c1
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-03-17 10:07:31 +0100

    a

--- src/Client1/View/LoginViewController.java
+++ src/Client1/View/LoginViewController.java
@@ -4,12 +4,16 @@ import Client1.ViewModel.ViewModelLogin;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 
 public class LoginViewController
 {
   private Scene scene;
   private ViewHandler viewHandler;
   private ViewModelLogin viewModelLogin;
+  @FXML TextField username;
+
+
   public void init(ViewHandler viewHandler,ViewModelLogin viewModelLogin,Scene scene)
   {
     this.viewHandler = viewHandler;
@@ -24,5 +28,9 @@ public class LoginViewController
 
   @FXML public void startChat()
   {
+
+    viewHandler.openChatView();
+
+
   }
 }
