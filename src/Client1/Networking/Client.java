8d44f49e0d87c01d6b340ddf69ecda68d92c9b2a
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-03-17 10:51:50 +0100

    a

--- src/Client1/Networking/Client.java
+++ src/Client1/Networking/Client.java
@@ -1,5 +1,36 @@
 package Client1.Networking;
 
-public class Client implements ClientModel
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client implements ClientModel, Runnable, PropertyChangeListener
 {
+
+    private String hostname = "localhost";
+    private int port;
+    private Socket socket;
+    private boolean running = true;
+
+    public Client(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(hostname, port);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        while(running){
+
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
---
+++ src/Server1/Server/PropertyChangeSubject.java
@@ -0,0 +1,21 @@
+package Server1.Server;
+
+import java.beans.PropertyChangeListener;
+
+public class PropertyChangeSubject {
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+
+    }
+
+    public void addPropertyChangeListener(String name, PropertyChangeListener listener){
+
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener){
+
+    }
+
+    public void removePropertyChangeListener(String name, PropertyChangeListener listener){
+
+    }
+}
