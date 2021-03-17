994f6a2e2aed6e14517c3a36f448aed29f793d54
Hansatang <portokarlos@gmail.com>
2021-03-17 11:25:36 +0100

    Pacierz 09:39

--- src/Client1/MODEL/DataModel.java
+++ src/Client1/MODEL/DataModel.java
@@ -3,4 +3,5 @@ package Client1.MODEL;
 public interface DataModel {
     void sendMessage(String text);
     void update();
+    void createClient();
 }
--- src/Client1/MODEL/DataModelManager.java
+++ src/Client1/MODEL/DataModelManager.java
@@ -1,21 +1,27 @@
 package Client1.MODEL;
 
+import Client1.Networking.Client;
+
 import java.beans.PropertyChangeSupport;
 
-public class DataModelManager implements DataModel {
+public class DataModelManager implements DataModel
+{
 
-    private PropertyChangeSupport support=new PropertyChangeSupport(this);
+  private Client client;
+  private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+  @Override public void sendMessage(String text)
+  {
 
+  }
 
+  @Override public void update()
+  {
 
-    @Override
-    public void sendMessage(String text) {
+  }
 
-    }
-
-    @Override
-    public void update() {
-
-    }
+  @Override public void createClient()
+  {
+    client = new Client(2910);
+  }
 }
