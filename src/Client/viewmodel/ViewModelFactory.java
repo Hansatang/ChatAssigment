36045697b63764338ae2bf9a7b8a12db4fea02ab
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-04-12 15:13:21 +0200

    Singleton fucker

--- src/Client/viewmodel/ViewModelFactory.java
+++ src/Client/viewmodel/ViewModelFactory.java
@@ -2,25 +2,54 @@ package Client.viewmodel;
 
 import Client.model.DataModel;
 
-public class ViewModelFactory
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ViewModelFactory implements Runnable
 {
+  private DataModel dataModel;
   private ViewModelLogin viewModelLogin;
   private ViewModelChat viewModelChat;
-  private DataModel dataModel;
 
-  public ViewModelFactory(DataModel dataModel)
-  {
+  private static ViewModelFactory instance;
+  private static Lock lock = new ReentrantLock();
+
+  public synchronized void setDataModel(DataModel dataModel){
     this.dataModel = dataModel;
-    this.viewModelChat = new ViewModelChat(dataModel);
+  }
+
+  public synchronized DataModel getDataModel(){
+    return this.dataModel;
+  }
+
+  public synchronized static ViewModelFactory getInstance(){
+    if(instance == null){
+      synchronized (lock){
+        instance = new ViewModelFactory();
+      }
+    }
+    return instance;
+  }
+
+  public synchronized void setModelLogin(ViewModelLogin viewModelLogin){
     this.viewModelLogin = new ViewModelLogin(dataModel);
   }
-  public ViewModelLogin getViewModelLogin()
-  {
+
+  public synchronized void setModelChat(ViewModelChat viewModelChat){
+    this.viewModelChat = new ViewModelChat(dataModel);
+  }
+
+  public synchronized ViewModelLogin getModelLogin(){
     return viewModelLogin;
   }
 
-  public ViewModelChat getViewModelChat()
-  {
+  public synchronized ViewModelChat getModelChat(){
     return viewModelChat;
+  }
+
+  @Override
+  public void run() {
+    while(true){
+    }
   }
 }
