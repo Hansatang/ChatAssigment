e9d6e03f71565f3eaf48517d35022e3ea2e9a5cd
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-04-13 11:05:05 +0200

    Singleton fucker

--- src/Client/viewmodel/ViewModelFactory.java
+++ src/Client/viewmodel/ViewModelFactory.java
@@ -7,22 +7,27 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ViewModelFactory
 {
+  /** Initialize ViewModel object **/
   private ViewModelLogin viewModelLogin;
   private ViewModelChat viewModelChat;
 
+  /** Initialize singleton and lock variables **/
   private static ViewModelFactory instance;
   private static Lock lock = new ReentrantLock();
 
+  /** Constructor to instantiate ViewModel objects which takes a DataModel object as parameter **/
   private ViewModelFactory(DataModel dataModel)
   {
     this.viewModelLogin = new ViewModelLogin(dataModel);
     this.viewModelChat = new ViewModelChat(dataModel);
   }
 
+  /** Singleton method to return an object of this class **/
   public synchronized static ViewModelFactory getInstance(DataModel dataModel)
   {
     if (instance == null)
     {
+      /** Lock to ensure that there won't be more classes able to get information the same time. **/
       synchronized (lock)
       {
         if (instance == null)
@@ -34,11 +39,13 @@ public class ViewModelFactory
     return instance;
   }
 
+  /** Get method for ViewModels, this returns ViewModelLogin **/
   public synchronized ViewModelLogin getModelLogin()
   {
     return viewModelLogin;
   }
 
+  /** Get method for ViewModels, this returns ViewModelChat **/
   public synchronized ViewModelChat getModelChat()
   {
     return viewModelChat;
