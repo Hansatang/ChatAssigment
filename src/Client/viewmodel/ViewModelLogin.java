21575f015bccd3ca2f893def7ccdcd3daeeb1356
Fogh1206 <esbenvensel.fogh@gmail.com>
2021-04-13 11:10:10 +0200

    Comments

--- src/Client/viewmodel/ViewModelLogin.java
+++ src/Client/viewmodel/ViewModelLogin.java
@@ -6,20 +6,25 @@ import javafx.beans.property.StringProperty;
 
 public class ViewModelLogin
 {
+
+  /** Declaring objects **/
   private StringProperty username;
   private DataModel model;
 
+  /** Constructor which initialize username and saves the parameter to a declared DataModel object **/
   public ViewModelLogin(DataModel model)
   {
     this.model = model;
     username = new SimpleStringProperty();
   }
 
+  /** Get method to retrieve username **/
   public StringProperty usernameProperty()
   {
     return username;
   }
 
+  /** Checks if username will by custom or anonymous and then starts chat with username **/
   public void startChat()
   {
     if (username.get() == null)
